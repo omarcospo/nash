d7bb1abce1716b2058e088b4f36863f76d50b262
Marcos <marcos@localhost.localdomain>
2023-10-23 00:09:09 -0300

    update

--- main.nim
+++ main.nim
@@ -1,3 +1,24 @@
+import nativesockets, osproc, std/paths, strutils
+
+let host = getHostname()
+let pwd = getCurrentDir().string
+let prompt = "[" & host & "] " & pwd & " "
+
+proc cd(input: string): string =
+  if startsWith(input, "cd"):
+    let splited = split(input, " ")
+    write(stdout, "that you path: " & splited[1] & "\n")
+
+
 while true:
-  write(stdout,"prompt >> ")
+  # -> PROMPT
+  write(stdout, prompt)
   let input = readLine(stdin)
+  # -> INPUT
+  let result = execProcess("dash -c " & "\"" & input & "\"")
+  write(stdout, result)
+  let cd = cd(input)
+  #CASE
+  case input
+  of "exit":
+      break
